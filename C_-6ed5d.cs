1d2da56f85256907cdcb9702b8ca2c693819fa32
daniel-locatelli <daniel.nlocatelli@gmail.com>
2023-06-21 15:48:51 +0200

    improved the structure of the code

--- C_-6ed5d.cs
+++ C_-6ed5d.cs
@@ -25,6 +25,9 @@ using GH_IO;
 using GH_IO.Serialization;
 using KangarooSolver;
 using KangarooSolver.Goals;
+using Rhino.Render.ChangeQueue;
+using Rhino.Runtime.InProcess;
+using Rhino.Render;
 
 
 /// <summary>
@@ -67,14 +70,90 @@ public abstract class Script_Instance_6ed5d : GH_ScriptInstance
   /// they will have a default value.
   /// </summary>
   #region Runscript
-  private void RunScript(Curve boundary, double offset, List<Curve> negative, List<Point3d> fixedColumns, int numberColumns, List<object> fixedWalls, double radius, ref object goals, ref object iterations)
+  private void RunScript(Curve boundary, double offset, List<Curve> negative, List<Point3d> fixedColumns, int numberColumns, List<Curve> fixedWalls, double radius, bool reset, bool run, ref object points, ref object iterations)
   {
-    /////////////////////////////
-    //initialize the solver
-    var PS = new KangarooSolver.PhysicalSystem();
-    var allGoals = new List<IGoal>();
+    /*
+        //add the goals to the solver
+        foreach (IGoal goal in allGoals)
+        {
+          PS.AssignPIndex(goal, 0.0001);
+          Print(goal.ToString());
+        }
+    */
 
-    ///////////////////////////
+    if (reset || PS.ParticleCount() > numberColumns)
+    {
+      PS.ClearParticles();
+      allGoals.Clear();
+      counter = 0;
+
+      //get brep boundary
+      Brep brepBoundary = getBoundary(offset, boundary, negative);
+
+      //get columns
+      allColumns = getColumns(brepBoundary, numberColumns, fixedColumns);
+
+      //convert brep to mesh
+      Rhino.Geometry.Mesh meshBoundary = Rhino.Geometry.Mesh.CreateFromBrep(brepBoundary, MeshingParameters.Default)[0];
+
+      //get goals
+      allGoals = getGoals(allColumns, fixedColumns, fixedWalls, meshBoundary, radius);
+
+      foreach (IGoal goal in allGoals) //Assign indexes to the particles in each Goal:
+      {
+        PS.AssignPIndex(goal, 0.0001); // the second argument is the tolerance distance below which points combine into a single particle
+        Print(goal.ToString());
+      }
+    } else
+    {
+      //get brep boundary
+      Brep brepBoundary = getBoundary(offset, boundary, negative);
+
+      //get columns
+      allColumns = getColumns(brepBoundary, numberColumns, fixedColumns);
+
+      //convert brep to mesh
+      Rhino.Geometry.Mesh meshBoundary = Rhino.Geometry.Mesh.CreateFromBrep(brepBoundary, MeshingParameters.Default)[0];
+
+      //get goals
+      allGoals = getGoals(allColumns, fixedColumns, fixedWalls, meshBoundary, radius);
+
+      foreach (IGoal goal in allGoals) //Assign indexes to the particles in each Goal:
+      {
+        PS.AssignPIndex(goal, 0.0001); // the second argument is the tolerance distance below which points combine into a single particle
+        //Print(goal.ToString());
+      }
+    }
+
+
+    int substeps = 5;
+
+    if (run) //PS.GetvSum() > 1e-15 &&
+    {
+      PS.MomentumStep(allGoals, 0.99, substeps);
+      counter += substeps;
+      Component.ExpireSolution(true);
+    }
+
+    //get the positions of the points
+    points = PS.GetPositions();
+
+    //get the number of iterations
+    iterations = PS.GetIterations();
+
+    //Print(allGoals.Count.ToString());
+
+  }
+  #endregion
+  #region Additional
+  List<Point3d> allColumns = new List<Point3d>();
+  KangarooSolver.PhysicalSystem PS = new KangarooSolver.PhysicalSystem();
+  List<IGoal> allGoals = new List<IGoal>();
+  int counter = 0;
+  //double threshold = 1e-15;
+
+  public Brep getBoundary(double offset, Curve boundary, List<Curve> negative)
+  {
     //if offset is positive, then make it negative
     if (offset > 0)
     {
@@ -98,11 +177,11 @@ public abstract class Script_Instance_6ed5d : GH_ScriptInstance
     //generate a surface with offseted boundary
     Brep brepBoundary = Brep.CreatePlanarBreps(boundaryCurves, 0.5)[0];
 
-    //convert brep to mesh
-    Mesh meshBoundary = Mesh.CreateFromBrep(brepBoundary, MeshingParameters.Default)[0];
-
-
-    ///////////////////////////
+    return brepBoundary;
+  }
+ 
+  private List<Point3d> getColumns(Brep brepBoundary, int numberColumns, List<Point3d> fixedColumns)
+  {
     //find the center of the boundary
     Point3d center = brepBoundary.GetBoundingBox(false).Center;
 
@@ -126,9 +205,11 @@ public abstract class Script_Instance_6ed5d : GH_ScriptInstance
       allColumns.Add(fixedColumns[i]);
     }
 
-    ///////////////////////////
-    //add goals to the list
-
+    return allColumns;
+  }
+  
+  private List<IGoal> getGoals(List<Point3d> allColumns, List<Point3d> fixedColumns, List<Curve> fixedWalls, Rhino.Geometry.Mesh meshBoundary, double radius)
+  {
     //onmesh goal
     OnMesh onMesh = new OnMesh(allColumns, meshBoundary, 1000);
     allGoals.Add(onMesh);
@@ -144,35 +225,19 @@ public abstract class Script_Instance_6ed5d : GH_ScriptInstance
       allGoals.Add(anchor);
     }
 
-    //add the goals to the solver
-    foreach (IGoal goal in allGoals)
+    //Add fixedWalls constraint here
+/*
+    List<object> collideList = new List<object>();
+    collideList.Add(allColumns);
+    collideList.Add(fixedWalls);
+    List<double> radiusCollide = new List<double>();
+    for (int i = 0; i < collideList.Count; i++)
     {
-      PS.AssignPIndex(goal, 0.0001);
-      Print(goal.ToString());
+      radiusCollide.Add(radius);
     }
+    Collider collide = new Collider(collideList, radiusCollide, null, null, 1);*/
 
-    int counter = 0;
-    double threshold = 1e-15;
-
-    do
-    {
-      //Step forward, using these goals, with multi-threading on, and stopping if the threshold is reached
-      PS.Step(allGoals, true, threshold);
-      counter++;
-    } while (PS.GetvSum() > threshold && counter < 1000);
-
-    //GetvSum returns the current kinetic energy
-    //always include a counter to prevent it getting stuck forever in case it cannot reach the given threshold
-
-    //get the positions of the points
-    goals = PS.GetPositions();
-
-    //get the number of iterations
-    iterations = PS.GetIterations();
-
+    return allGoals;
   }
-  #endregion
-  #region Additional
-
   #endregion
 }
